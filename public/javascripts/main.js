998eccc4a6f4f8ce5b91fd3eb34eaf1d803cedd2
jsablaon <jsablaon644@gmail.com>
2022-04-09 14:43:08 -0700

    code cleanup

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,4 @@
-
-// let orderArray = [];
+// key value pairs for stores and employees
 let storeEmployeeObjects = {
     98053: [1,2,3,4],
     98007: [5,6,7,8],
@@ -9,19 +8,9 @@ let storeEmployeeObjects = {
     98046: [21,22,23,24]
 };
 
-// console.log(Object.keys(storeEmployeeObjects));
-// console.log(storeEmployeeObjects[98011]);
 let keys = Object.keys(storeEmployeeObjects);
 let randomStoreNumber = keys[keys.length * Math.random() << 0]
-// console.log(randomStoreNumber, "randomStoreNumber");
 let randomEmployeeId = storeEmployeeObjects[randomStoreNumber].length * Math.random() << 0;
-// console.log(randomEmployeeId, "randomEmployeeId")
-// console.log(storeEmployeeObjects[randomStoreNumber][randomEmployeeId], "storeEmployeeIds");
-
-
-// console.log(storeEmployeeObjects[keys[ keys.length * Math.random() << 0]]);
-
-// (98053 , 98007, 98077, 98055, 98011, 98046)
 
 // define a constructor to create order objects
 let OrderObject = function (pStoreId, pSalesPersonId, pCdId, pPricePaid, pDate) {
@@ -33,16 +22,11 @@ let OrderObject = function (pStoreId, pSalesPersonId, pCdId, pPricePaid, pDate) 
 }
 
 let initialDateTime = Date.now();
-
-let newOrder = new OrderObject(randomStoreNumber, storeEmployeeObjects[randomStoreNumber][randomEmployeeId], 1,1, initialDateTime);
-console.log(newOrder, "newOrder");
-
-// random CdID
-// (123456, 123654, 321456, 321654, 654123, 654321, 543216, 354126, 621453, 623451)
+let newOrder;
 let CdIDs = [123456, 123654, 321456, 321654, 654123, 654321, 543216, 354126, 621453, 623451];
+
 function getRandomCd(){
     let randomIndex = getRandomIntInclusive(0, CdIDs.length-1);
-    console.log(randomIndex, "random index for cd");
     return CdIDs[randomIndex];
 }
 
@@ -64,43 +48,50 @@ function generateRandomOrder(){
 
 function createDisplay(){
     generateRandomOrder();
-    console.log(newOrder.StoreID, newOrder.SalesPersonID, newOrder.CdID, newOrder.PricePaid, newOrder.Date, "logging new order object");
-
     let divOrderList = document.getElementById("divOrderList");
-
-    divOrderList.innerHTML = `StoreID = ${newOrder.StoreID} \n
-                                SalesPersonID = ${newOrder.SalesPersonID} \n
-                                CdID = ${newOrder.CdID} \n
-                                PricePaid = ${newOrder.PricePaid} \n
-                                Date = ${newOrder.Date} \n`
+    divOrderList.innerHTML = `StoreID = ${newOrder.StoreID} <br>
+                                SalesPersonID = ${newOrder.SalesPersonID} <br>
+                                CdID = ${newOrder.CdID} <br>
+                                PricePaid = ${newOrder.PricePaid} <br>
+                                Date = ${newOrder.Date} <br>`
 }
 
-function addOneToServer(){
+function addOneToServer(isOneOrder = true){
     createDisplay();
-    fetch('/AddOrder', {
-        method: "POST",
-        body: JSON.stringify(newOrder),
-        headers: {"Content-type": "application/json; charset=UTF-8"}
-        })
-        .then(response => response.json()) 
-        .then(json => console.log(json)
-        )
-        .catch(err => console.log(err));
+
+    if(isOneOrder){
+        console.log("adding a single order")
+        fetch('/AddOrder', {
+            method: "POST",
+            body: JSON.stringify(newOrder),
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+            })
+            .then(response => response.json()) 
+            .then(json => console.log(json)
+            )
+            .catch(err => console.log(err));
+    } else {
+        console.log("adding 500 orders")
+        fetch('/Add500Order', {
+            method: "POST",
+            body: JSON.stringify(newOrder),
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+            })
+            .then(response => response.json()) 
+            .then(json => console.log(json)
+            )
+            .catch(err => console.log(err));
+    }
 }
 
 function add500ToServer(){
     for (let i = 0; i < 500; i++) {
-        console.log(i + 1, "count of orders");
-        addOneToServer();
+        addOneToServer(false);
     }
 }
 
-
-let selectedGenre = "not selected";
-
 document.addEventListener("DOMContentLoaded", function () {
 
-    // added by me
     document.getElementById("buttonCreate").addEventListener("click", function (){
         createDisplay();
     });
@@ -112,143 +103,5 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("buttonAdd500").addEventListener("click", function (){
         add500ToServer();
     })
-
-    // end of my addition ======================================
-
-    createList();
-
-// add button events ************************************************************************
-    
-    document.getElementById("buttonAdd").addEventListener("click", function () {
-        let newMovie = new MovieObject(document.getElementById("title").value, 
-        document.getElementById("year").value, selectedGenre);
-
-        fetch('/AddMovie', {
-            method: "POST",
-            body: JSON.stringify(newMovie),
-            headers: {"Content-type": "application/json; charset=UTF-8"}
-            })
-            .then(response => response.json()) 
-            .then(json => console.log(json),
-            createList()
-            )
-            .catch(err => console.log(err));
-    
-        // $.ajax({
-        //     url : "/AddMovie",
-        //     type: "POST",
-        //     data: JSON.stringify(newMovie),
-        //     contentType: "application/json; charset=utf-8",
-        //      success: function (result) {
-        //         console.log(result);
-        //         createList();
-        //     }
-        // });
-       
-    });
-
-    document.getElementById("buttonGet").addEventListener("click", function () {
-        createList();      
-    });
-
-    document.getElementById("buttonDelete").addEventListener("click", function () {
-        deleteMovie(document.getElementById("deleteID").value);      
-    });
-    
-    document.getElementById("buttonClear").addEventListener("click", function () {
-        document.getElementById("title").value = "";
-        document.getElementById("year").value = "";
-        createList();   
-    });
-
-    $(document).bind("change", "#select-genre", function (event, ui) {
-        selectedGenre = $('#select-genre').val();
-    });
-
-  
-
 });  
 // end of wait until document has loaded event  *************************************************************************
-
-
-function createList() {
-// update local array from server
-
-    fetch('/getAllMovies')
-    // Handle success
-    .then(response => response.json())  // get the data out of the response object
-    .then( responseData => fillUL(responseData))    //update our array and li's
-    .catch(err => console.log('Request Failed', err)); // Catch errors
-
-    // $.get("/getAllMovies", function(data, status){  // AJAX get
-    //     movieArray = data;  // put the returned server json data into our local array
-        
-    //       // clear prior data
-    //     var divMovieList = document.getElementById("divMovieList");
-    //     while (divMovieList.firstChild) {    // remove any old data so don't get duplicates
-    //         divMovieList.removeChild(divMovieList.firstChild);
-    //     };
-
-    //     var ul = document.createElement('ul');
-
-    //     movieArray.forEach(function (element,) {   // use handy array forEach method
-    //         var li = document.createElement('li');
-    //         li.innerHTML = element.ID + ":  &nbsp &nbsp  &nbsp &nbsp " + 
-    //         element.Title + "  &nbsp &nbsp  &nbsp &nbsp "  
-    //         + element.Year + " &nbsp &nbsp  &nbsp &nbsp  " + element.Genre;
-    //         ul.appendChild(li);
-    //     });
-    //     divMovieList.appendChild(ul)
-
-    // });
-};
-
-function fillUL(data) {
-    movieArray = data;
-    
-    // clear prior data
-    var divMovieList = document.getElementById("divMovieList");
-    while (divMovieList.firstChild) {    // remove any old data so don't get duplicates
-        divMovieList.removeChild(divMovieList.firstChild);
-    };
-
-    var ul = document.createElement('ul');
-    
-    movieArray.forEach(function (element,) {   // use handy array forEach method
-        var li = document.createElement('li');
-        li.innerHTML = element.ID + ":  &nbsp &nbsp  &nbsp &nbsp " + 
-        element.Title + "  &nbsp &nbsp  &nbsp &nbsp "  
-        + element.Year + " &nbsp &nbsp  &nbsp &nbsp  " + element.Genre;
-        ul.appendChild(li);
-    });
-    divMovieList.appendChild(ul)
-}
-
-function deleteMovie(ID) {
-
-    fetch('/DeleteMovie/' + ID, {
-        method: "DELETE",
-       // body: JSON.stringify(_data),
-        headers: {"Content-type": "application/json; charset=UTF-8"}
-      })
-      .then(response => response.json()) 
-      .then(json => console.log(json), createList())
-      .catch(err => console.log(err));
-
-
-    // $.ajax({
-    //     type: "DELETE",
-    //     url: "/DeleteMovie/" +ID,
-    //     success: function(result){
-    //         alert(result);
-    //         createList();
-    //     },
-    //     error: function (xhr, textStatus, errorThrown) {  
-    //         alert("Server could not delete Movie with ID " + ID)
-    //     }  
-    // });
-   
-}
-
-
-  
--- routes/index.js
+++ routes/index.js
@@ -14,23 +14,6 @@ let ServerOrderObject = function (pStoreId, pSalesPersonId, pCdId, pPricePaid, p
 
 // my file management code, embedded in an object
 fileManagerOrder  = {
-
-  // this will read a file and put the data in our movie array
-  // NOTE: both read and write files are synchonous, we really can't do anything
-  // useful until they are done.  If they were async, we would have to use call backs.
-  // functions really should take in the name of a file to be more generally useful
-  read: function() {
-    // has extra code to add 4 movies if and only if the file is empty
-    const stat = fs.statSync('ordersData.json');
-    if (stat.size !== 0) {                           
-    var rawdata = fs.readFileSync('ordersData.json'); // read disk file
-    ServerOrderArray = JSON.parse(rawdata);  // turn the file data into JSON format and overwrite our array
-    }
-    else {
-      fileManagerOrder.write();
-    }
-  },
-  
   write: function() {
     let data = JSON.stringify(ServerOrderArray);    // take our object data and make it writeable
     fs.writeFileSync('ordersData.json', data);  // write it
@@ -40,63 +23,19 @@ fileManagerOrder  = {
 // start by creating data so we don't have to type it in each time
 let ServerMovieArray = [];
 
-// define a constructor to create movie objects
-let MovieObject = function (pTitle, pYear, pGenre) {
-    this.ID = Math.random().toString(16).slice(5)  // tiny chance could get duplicates!
-    this.Title = pTitle;
-    this.Year = pYear;
-    this.Genre = pGenre;  // action  comedy  drama  horrow scifi  musical  western
-}
-
-// my file management code, embedded in an object
-fileManager  = {
-
-  // this will read a file and put the data in our movie array
-  // NOTE: both read and write files are synchonous, we really can't do anything
-  // useful until they are done.  If they were async, we would have to use call backs.
-  // functions really should take in the name of a file to be more generally useful
-  read: function() {
-    // has extra code to add 4 movies if and only if the file is empty
-    const stat = fs.statSync('moviesData.json');
-    if (stat.size !== 0) {                           
-    var rawdata = fs.readFileSync('moviesData.json'); // read disk file
-    ServerMovieArray = JSON.parse(rawdata);  // turn the file data into JSON format and overwrite our array
-    }
-    else {
-      // make up 3 for testing
-      ServerMovieArray.push(new MovieObject("Moonstruck", 1981, "Drama"));
-      ServerMovieArray.push(new MovieObject("Wild At Heart", 1982, "Drama"));
-      ServerMovieArray.push(new MovieObject("Raising Arizona", 1983, "Comedy"));
-      ServerMovieArray.push(new MovieObject("USS Indianapolis", 2016, "Drama"));
-      fileManager.write();
-    }
-  },
-  
-  write: function() {
-    let data = JSON.stringify(ServerMovieArray);    // take our object data and make it writeable
-    fs.writeFileSync('moviesData.json', data);  // write it
-  },
-}
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile('index.html');
 });
 
-/* GET all Movie data */
-router.get('/getAllMovies', function(req, res) {
-  fileManager.read();
-  res.status(200).json(ServerMovieArray);
-});
-
-
-/* Add one new Movie */
-router.post('/AddMovie', function(req, res) {
-  const newMovie = req.body;  // get the object from the req object sent from browser
-  console.log(newMovie);
-  ServerMovieArray.push(newMovie);  // add it to our "DB"  (array)
-  fileManager.write();
+/* Add 500 new Order */
+router.post('/Add500Order', function(req, res) {
+  const newOrder = req.body;  // get the object from the req object sent from browser
+  let newOrderObject = new ServerOrderObject(newOrder.StoreID, newOrder.SalesPersonID, newOrder.CdID, newOrder.PricePaid, newOrder.Date);
+  console.log(newOrderObject, "new order object");
+  ServerOrderArray.push(newOrderObject);  // add it to our "DB"  (array)
+  console.log(`added 1 to server array ${ServerOrderArray.length}`);
+  fileManagerOrder.write();
   // prepare a reply to the browser
   var response = {
     status  : 200,
@@ -110,12 +49,8 @@ router.post('/AddOrder', function(req, res) {
   const newOrder = req.body;  // get the object from the req object sent from browser
   let newOrderObject = new ServerOrderObject(newOrder.StoreID, newOrder.SalesPersonID, newOrder.CdID, newOrder.PricePaid, newOrder.Date);
   console.log(newOrderObject, "new order object");
-  // ServerOrderArray = []; // clear array
-  console.log(`cleared server array ${ServerOrderArray.length}`)
   ServerOrderArray.push(newOrderObject);  // add it to our "DB"  (array)
   console.log(`added 1 to server array ${ServerOrderArray.length}`);
-  fileManagerOrder.write();
-  // fileManager.write();
   // prepare a reply to the browser
   var response = {
     status  : 200,
@@ -123,37 +58,5 @@ router.post('/AddOrder', function(req, res) {
   }
   res.end(JSON.stringify(response)); // send reply
 });
-
-// delete movie
-
-router.delete('/DeleteMovie/:ID', (req, res) => {
-  const ID = req.params.ID;
-  let found = false;
-  console.log(ID);    
-
-  for(var i = 0; i < ServerMovieArray.length; i++) // find the match
-  {
-      if(ServerMovieArray[i].ID === ID){
-        ServerMovieArray.splice(i,1);  // remove object from array
-          found = true;
-          fileManager.write();
-          break;
-      }
-  }
-
-  if (!found) {
-    console.log("not found");
-    return res.status(500).json({
-      status: "error"
-    });
-  } else {
-    var response = {
-      status  : 200,
-      success : 'Movie ' + ID + ' deleted!'
-    }
-    res.end(JSON.stringify(response)); // send reply
-  }
-});
-
 
 module.exports = router;
